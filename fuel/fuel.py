c666b8e038b4b938adad3f0d5971ffe8515b46c0
Kimmicorn-glitch <kimberley.bezuidenhout@gmail.com>
2025-08-29 12:20:35 +0200

    removed the unnecesarry break. fixed indentation. added a try and except block

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,30 +4,32 @@ def main():
             fraction = input("Fraction: ").strip()
             percent= convert(fraction)
             print(gauge(percent))
-            break
+            
         except (ValueError, ZeroDivisionError):
-            continue
+            pass
 
 def convert(fraction):
-       
-    x , y = fraction.split("/")
-    x = int(x)
-    y = int(y)
+    try:   
+        x , y = fraction.split("/")
+        x = int(x)
+        y = int(y)
     
     
-    if y == 0 or x < 0 or y < 0 or x > y:
+        if y == 0 or x < 0 or y < 0 or x > y:
+            raise ValueError
+        return round((x/y)*100)
+    except (ValueError, ZeroDivisionError):
         raise ValueError
-                
-    return round((x/y)*100)
+    
 
             
-def gauge(percent):           
-    if percent <= 1:
+def gauge(percentage):           
+    if percentage <= 1:
         return "E"
-    elif percent >=99:
+    elif percentage >=99:
         return "F"
     else:
-        return(f"{percent}%")
+        return(f"{percentage}%")
       
 if __name__=="__main__":
     main()

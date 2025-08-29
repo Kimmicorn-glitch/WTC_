22f9e35d0806d904b38d0dfaa78d6c065a6bc0d8
Kimmicorn-glitch <kimberley.bezuidenhout@gmail.com>
2025-08-29 12:04:14 +0200

    finally fixed the mess i made. INDENTATIONS !!!

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,29 +1,30 @@
 def main():
-     try: 
-        fraction = input("Fraction").strip()\
-        percentage = convert(fraction)
-        print(gauge(percent))
-         break
-except (ValueError, ZeroDivisionError)
-    continue
+    while True:     
+        try: 
+            fraction = input("Fraction: ").strip()
+            percent= convert(fraction)
+            print(gauge(percent))
+            break
+        except (ValueError, ZeroDivisionError):
+            continue
 
 def convert(fraction):
        
-            x , y = fraction.split("/")
-            x = int(x)
-            y = int(y)
-            if y == 0 or x < 0 or y < 0:
-                raise ValueError
+    x , y = fraction.split("/")
+    x = int(x)
+    y = int(y)
+    
+    
+    if y == 0 or x < 0 or y < 0 or x > y:
+        raise ValueError
                 
-           return round((x/y)*100)
+    return round((x/y)*100)
 
-        except (ValueError, ZeroDivisionError):
-            raise ValueError
             
-def gauge(percentage):           
+def gauge(percent):           
     if percent <= 1:
         return "E"
-     elif percent >=99:
+    elif percent >=99:
         return "F"
     else:
         return(f"{percent}%")

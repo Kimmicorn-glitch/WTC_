a2f9f2a4815b5b323690098f727ef53b8405e57c
Kimmicorn-glitch <kimberley.bezuidenhout@gmail.com>
2025-09-07 12:57:52 +0200

    week 8 problem set 1

---
+++ seasons/seasons.py
@@ -0,0 +1,29 @@
+from datetime import date
+import sys
+import inflect
+
+def main():
+    birthday_str = input("Enter your date of birth: ")
+    try:
+        birthday = date.fromisoformat(birthday_str)
+    except ValueError:
+        sys.exit("Invalid date")
+
+    minutes = age_in_min(birthday)
+    print(minutes_to_words(minutes))
+
+def age_in_min(birthday):
+    today= date.today()
+    diff = today - birthday
+    return diff.days * 24 *60
+
+
+def minutes_to_words(minutes):
+    p = inflect.engine()
+    words = p.number_to_words(minutes, andword="")
+
+    return words.capitalize() + " minutes"
+
+
+if __name__ == "__main__":
+    main()
